20636e2e81b8b917e5f81b9408bcd546ef4197c4
Jack <ja.wegert>
2020-11-10 12:21:33 +0100

    Expand user class.

--- CoreServices/User/src/main/java/com/core/userservice/User.java
+++ CoreServices/User/src/main/java/com/core/userservice/User.java
@@ -1,20 +1,74 @@
 package com.core.userservice;
 
+public enum Role{
+    USER, ADMIN
+}
+
 public class User {
 
-	private final long id;
-	private final String content;
+	private int id;
+    private String username;
+    private String firstname;
+    private String lastname;
+    private String password;
+    private Role role;
 
-	public User(long id, String content) {
+
+    public User(long id, String username, String firstname, String lastname, String password, Role role) {
 		this.id = id;
-		this.content = content;
+        this.username= username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.password = password;
+        this.role = role;
 	}
 
 	public long getId() {
 		return id;
+    }
+    
+    public void setId(int id) {
+		this.id = id;
 	}
 
-	public String getContent() {
-		return content;
-	}
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname){
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname){
+        this.lastname = lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassowrd(String password){
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(Role role){
+        this.role = role;
+    }
+    
 }
