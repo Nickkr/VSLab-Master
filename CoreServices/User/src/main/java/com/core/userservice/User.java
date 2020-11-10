08ac9399f593938700c2e9df01a300ab69d5bfd6
Jack <ja.wegert>
2020-11-10 12:02:41 +0100

    Rename file.

--- CoreServices/User/src/main/java/com/core/userservice/User.java
+++ CoreServices/User/src/main/java/com/core/userservice/User.java
@@ -1,11 +1,11 @@
 package com.core.userservice;
 
-public class Greeting {
+public class User {
 
 	private final long id;
 	private final String content;
 
-	public Greeting(long id, String content) {
+	public User(long id, String content) {
 		this.id = id;
 		this.content = content;
 	}
