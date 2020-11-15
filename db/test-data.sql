22eeed6b2a22dbd5667acc24e31510e49c4e161f
Joachim Mees <mejo1031@hs-karlsruhe.de>
2020-11-15 17:34:34 +0100

    Corrected SQL errors in test-data and fixed UTF8 encoding during initialization.

--- db/test-data.sql
+++ db/test-data.sql
@@ -1,17 +1,14 @@
 -- Users
-insert into user.user (`name`, `lastname`, `password`, `username`, `role`) values('admin', 'admin', 'admin', 'admin', 1);
-insert into user.user (`name`, `lastname`, `password`, `username`, `role`) values('foo', 'bar', 'admin', 'foo', 1);
-insert into user.user (`name`, `lastname`, `password`, `username`, `role`) values('foo', 'bar', 'admin', 'foo', 0);
+insert into user.user (`name`, `lastname`, `password`, `username`, `role`) values ('admin', 'admin', 'admin', 'admin', 1);
+insert into user.user (`name`, `lastname`, `password`, `username`, `role`) values ('foo', 'bar', 'admin', 'foo-admin', 1);
+insert into user.user (`name`, `lastname`, `password`, `username`, `role`) values ('foo', 'bar', 'user', 'foo-user', 0);
 
 -- Categories
-
-insert into category.category (`id`, `name`) values(1, 'Obst')
-insert into category.category (`id`, `name`) values(2, 'Gemüse')
+insert into category.category (`id`, `name`) values (1, 'Obst');
+insert into category.category (`id`, `name`) values (2, 'Gemüse');
 
 -- Products
-
-insert into product.product (`details`, `name`, `price`, `categoryId`) values('This is a test detail.', 'Apfel', 2.0, 1)
-insert into product.product (`details`, `name`, `price`, `categoryId`) values('This is a test detail.', 'Kiwi', 3.0, 1)
-insert into product.product (`details`, `name`, `price`, `categoryId`) values('This is a test detail.', 'Banane', 1.0, 1)
-insert into product.product (`details`, `name`, `price`, `categoryId`) values('This is a test detail.', 'Karotte', 1.0, 2)
-
+insert into product.product (`details`, `name`, `price`, `category_id`) values ('This is a test detail.', 'Apfel', 2.0, 1);
+insert into product.product (`details`, `name`, `price`, `category_id`) values ('This is a test detail.', 'Kiwi', 3.0, 1);
+insert into product.product (`details`, `name`, `price`, `category_id`) values ('This is a test detail.', 'Banane', 1.0, 1);
+insert into product.product (`details`, `name`, `price`, `category_id`) values ('This is a test detail.', 'Karotte', 1.0, 2);
