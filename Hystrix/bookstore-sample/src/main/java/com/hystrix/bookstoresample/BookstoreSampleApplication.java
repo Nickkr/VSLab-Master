95230d5dc9e029d499e62bc9e1eab2f7e4a8c57e
Jack <ja.wegert>
2020-11-19 15:57:26 +0100

    Add sample project.

---
+++ Hystrix/bookstore-sample/src/main/java/com/hystrix/bookstoresample/BookstoreSampleApplication.java
@@ -0,0 +1,13 @@
+package com.hystrix.bookstoresample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreSampleApplication.class, args);
+	}
+
+}
---
+++ Hystrix/bookstore-sample/src/test/java/com/hystrix/bookstoresample/BookstoreSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hystrix.bookstoresample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
