2cf312f02f4829a278c59c9c1b16244a19bc7524
nkreu <nkreutzarek@hotmail.de>
2020-11-20 17:19:48 +0100

    Add zuul and eureka server

---
+++ Eureka/src/test/java/com/eureka/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eureka.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Disabled;
+
+
+@Disabled
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
