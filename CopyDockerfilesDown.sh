3b514a235ff58beb2fbae1f817ae14447a8304ca
Joachim Mees <mejo1031@hs-karlsruhe.de>
2020-11-23 15:53:40 +0100

    Updated CopyDockerfilesDown.sh script to perform recursive copy to all folders containing a maven pom.

--- CopyDockerfilesDown.sh
+++ CopyDockerfilesDown.sh
@@ -1,1 +1,7 @@
-cp .dockerignore BuildFromExecutableJar.dockerfile BuildFromMavenSources.dockerfile CoreServices/Category/
+#!/bin/sh
+
+OriginFiles=".dockerignore *.dockerfile"
+
+for dir in $(find . -mindepth 2 -name pom.xml -printf '%h\n'); do
+  cp --verbose --update $OriginFiles $dir
+done
