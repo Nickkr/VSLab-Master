211e3c3ef953485075227f9bcb6ec14a2923ac77
Joachim Mees <mejo1031@hs-karlsruhe.de>
2020-11-24 12:53:35 +0100

    Added double quotes in CopyDockerfilesDown.sh script.

--- CopyDockerfilesDown.sh
+++ CopyDockerfilesDown.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 ScriptDir="$(dirname "$0")"
-RootDir="$(realpath $ScriptDir/./)"
-OriginFiles="$(realpath $ScriptDir/.dockerignore $ScriptDir/*.dockerfile)"
+RootDir="$(realpath "$ScriptDir"/./)"
+OriginFiles="$(realpath "$ScriptDir"/.dockerignore "$ScriptDir"/*.dockerfile)"
 
-for dir in $(find $RootDir -mindepth 2 -name pom.xml -printf '%h\n'); do
-  cp --verbose --update $OriginFiles $dir
+for dir in $(find "$RootDir" -mindepth 2 -name pom.xml -printf '%h\n'); do
+  cp --verbose --update "$OriginFiles" "$dir"
 done
 
 read -p "Press enter to continue"
