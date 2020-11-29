dd4159a82d2d12c1a5ee9aa8f1e366fb41401aca
Joachim Mees <mejo1031@hs-karlsruhe.de>
2020-11-29 19:41:30 +0100

    Updated core services configuration and added embedded database for development with initialization scripts.

---
+++ CoreServices/Category/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+-- Categories
+insert into category (`id`, `name`) values (1, 'Obst');
+insert into category (`id`, `name`) values (2, 'Gemüse');
---
+++ CoreServices/User/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+-- Users
+insert into user (`name`, `lastname`, `password`, `username`, `role`) values ('admin', 'admin', 'admin', 'admin', 1);
+insert into user (`name`, `lastname`, `password`, `username`, `role`) values ('foo', 'bar', 'admin', 'foo-admin', 1);
+insert into user (`name`, `lastname`, `password`, `username`, `role`) values ('foo', 'bar', 'user', 'foo-user', 0);
