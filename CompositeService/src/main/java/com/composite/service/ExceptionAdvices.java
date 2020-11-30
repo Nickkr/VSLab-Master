d45c9f2e023d12a0d969bb208ec8d869d349fa84
Joachim Mees <mejo1031@hs-karlsruhe.de>
2020-11-30 23:04:56 +0100

    Refactored ExceptionAdvices to pass through the HTTP status code.

--- CompositeService/src/main/java/com/composite/service/ExceptionAdvices.java
+++ CompositeService/src/main/java/com/composite/service/ExceptionAdvices.java
@@ -1,29 +1,32 @@
 package com.composite.service;
 
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 @ControllerAdvice
 public class ExceptionAdvices {
 
 	@ResponseBody
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> GeneralExceptionHandler(Exception ex) {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.toString());
+	}
+
+	@ResponseBody
 	@ExceptionHandler(HttpClientErrorException.class)
-	@ResponseStatus(HttpStatus.NOT_FOUND)
-	String HttpClientErrorExceptionHandler(HttpClientErrorException ex) {
-		return ex.getMessage();
+	ResponseEntity<String> HttpClientErrorExceptionHandler(HttpClientErrorException ex) {
+		return ResponseEntity.status(ex.getStatusCode()).body(ex.getMessage());
 	}
 
 	@ResponseBody
 	@ExceptionHandler(WebClientResponseException.class)
-	@ResponseStatus(HttpStatus.NOT_FOUND)
-	String WebClientResponseExceptionHandler(WebClientResponseException ex) {
-		return ex.getMessage();
+	public ResponseEntity<String> WebClientResponseExceptionHandler(WebClientResponseException ex) {
+		return ResponseEntity.status(ex.getStatusCode()).body(ex.getMessage());
 	}
+
 }
