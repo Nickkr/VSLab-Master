99584d8999c4c8c201d31a32d8ccc49723563829
Joachim Mees <mejo1031@hs-karlsruhe.de>
2020-12-03 13:54:27 +0100

    Added constraint name in Database/initial.sql script.

--- Database/initial.sql
+++ Database/initial.sql
@@ -40,7 +40,7 @@ CREATE TABLE category (
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
 	PRIMARY KEY (id),
-	UNIQUE (name)
+	CONSTRAINT UniqueName UNIQUE (name)
 ) ENGINE=InnoDB;
 
 
