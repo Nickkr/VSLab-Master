794f2e64f525c7370540f7950198ca2a269a5498
Joachim Mees <mejo1031@hs-karlsruhe.de>
2020-12-04 00:22:04 +0100

    Changed ExceptionAdvices and CategoryController to use response body and status annotations.

--- CoreServices/Category/src/main/java/com/core/categoryservice/CategoryController.java
+++ CoreServices/Category/src/main/java/com/core/categoryservice/CategoryController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,30 +24,35 @@ public class CategoryController implements CategoryInterface {
 	private CategoryService service;
 
 	@GetMapping
+	@ResponseBody
 	@ResponseStatus(HttpStatus.OK)
 	public List<Category> getCategories() {
 		return service.getCategories();
 	}
 
 	@GetMapping(params = "searchName")
+	@ResponseBody
 	@ResponseStatus(HttpStatus.OK)
 	public List<Category> getFilteredCategories(@RequestParam String searchName) {
 		return service.getFilteredCategories(searchName);
 	}
 
 	@PostMapping
+	@ResponseBody
 	@ResponseStatus(HttpStatus.CREATED)
 	public Category createCategory(@RequestBody Category newCategory) {
 		return service.createCategory(newCategory);
 	}
 
 	@GetMapping("{id}")
+	@ResponseBody
 	@ResponseStatus(HttpStatus.OK)
 	public Category getCategory(@PathVariable Integer id) {
 		return service.getCategory(id);
 	}
 
 	@PutMapping("{id}")
+	@ResponseBody
 	@ResponseStatus(HttpStatus.CREATED)
 	public Category updateCategory(@PathVariable Integer id, @RequestBody Category newCategory) {
 		return service.updateCategory(id, newCategory);
