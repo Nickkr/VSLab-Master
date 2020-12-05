a3c9047ecbe1371046d0683d0db4d8545bf4fde2
Joachim Mees <mejo1031@hs-karlsruhe.de>
2020-12-05 20:46:44 +0100

    Implemented filtering from cache and removed annotations in CategoryDelegateService.

--- CompositeService/src/main/java/com/composite/service/CategoryDelegateService.java
+++ CompositeService/src/main/java/com/composite/service/CategoryDelegateService.java
@@ -1,7 +1,10 @@
 package com.composite.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -10,17 +13,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -34,85 +28,85 @@ public class CategoryDelegateService implements CategoryDelegateInterface {
 	private static final String PRODUCT_BASE_URL = "http://product-service/products";
 
 	@Autowired
+	private CategoryCache cache;
+
+	@Autowired
 	@LoadBalanced
 	private WebClient.Builder webClientBuilder;
+
 	@Autowired
 	@LoadBalanced
 	private RestTemplate restTemplate;
 
 	@SuppressWarnings("rawtypes")
-	@HystrixCommand(fallbackMethod = "getCategoriesFallback")
-	@GetMapping
 	public ResponseEntity<List> getCategories() {
 		return restTemplate.getForEntity(CATEGORY_BASE_URL, List.class);
 	}
 
 	@SuppressWarnings("rawtypes")
 	public ResponseEntity<List> getCachedCategories() {
-		return ResponseEntity.ok(cache);
+		List<Category> body = new ArrayList<Category>(cache.values());
+		return ResponseEntity.ok(body);
 	}
 
 	@SuppressWarnings("rawtypes")
-	@HystrixCommand(fallbackMethod = "getFilteredCategoriesFallback")
-	@GetMapping(params = "searchName")
-	public ResponseEntity<List> getFilteredCategories(@RequestParam String searchName) {
+	public ResponseEntity<List> getFilteredCategories(String searchName) {
 		return restTemplate.getForEntity(CATEGORY_BASE_URL + "?searchName={id}", List.class, searchName);
 	}
 
 	@SuppressWarnings("rawtypes")
 	public ResponseEntity<List> getCachedFilteredCategories(String searchName) {
-		return ResponseEntity.ok(cache);
+		// Includes a category if its name contains the search name case insensitive.
+		Predicate<Category> matcher = category -> {
+			return category.getName().toLowerCase().contains(searchName.toLowerCase());
+		};
+
+		List<Category> body = cache.values().stream().filter(matcher).collect(Collectors.toList());
+		return ResponseEntity.ok(body);
 	}
 
-	@HystrixCommand
-	@PostMapping
-	public ResponseEntity<Category> createCategory(@RequestBody Category newCategory) {
+	public ResponseEntity<Category> createCategory(Category newCategory) {
 		return restTemplate.postForEntity(CATEGORY_BASE_URL, newCategory, Category.class);
 	}
 
-	@HystrixCommand(fallbackMethod = "getCategoryFallback")
-	@GetMapping("{id}")
-	public ResponseEntity<Category> getCategory(@PathVariable Integer id) {
+	public ResponseEntity<Category> getCategory(Integer id) {
 		return restTemplate.getForEntity(CATEGORY_BASE_URL + "/{id}", Category.class, id);
 	}
 
 	public ResponseEntity<Category> getCachedCategory(Integer id) {
-		return null;
+		Category body = cache.get(id);
+		return ResponseEntity.ok(body);
 	}
 
-	@HystrixCommand
-	@PutMapping("{id}")
-	public ResponseEntity<Category> updateCategory(@PathVariable Integer id, @RequestBody Category newCategory) {
+	public ResponseEntity<Category> updateCategory(Integer id, Category newCategory) {
 		return restTemplate.exchange(CATEGORY_BASE_URL + "/{id}", HttpMethod.PUT, new HttpEntity<Category>(newCategory), Category.class, id);
 	}
 
-	@HystrixCommand
-	@DeleteMapping("{id}")
-	public ResponseEntity<Void> deleteCategory(@PathVariable Integer id) {
-	
+	public ResponseEntity<Void> deleteCategory(Integer id) {
+
 		final WebClient categoryClient = webClientBuilder.baseUrl(CATEGORY_BASE_URL).build();
 		final WebClient productClient = webClientBuilder.baseUrl(PRODUCT_BASE_URL).build();
-	
+
 		// Return failure if the category does not exists.
 		if (getCategory(id).getStatusCode() != HttpStatus.OK) {
 			return ResponseEntity.notFound().build();
 		}
-	
+
 		// Get all product for the given category id.
 		Product[] products = productClient.get().uri("?categoryId={id}", id).retrieve().bodyToMono(Product[].class).block();
-	
+
 		// Function call to delete a single product.
 		Function<Product, Mono<ResponseEntity<Void>>> deleteProductByID = (product) -> {
 			return productClient.delete().uri("/{id}", product.getId()).retrieve().toBodilessEntity();
 		};
-	
+
 		// Delete all products by its id and throw exceptions if an error occurs.
 		ParallelFlux<Product> parallel = Flux.fromArray(products).parallel().runOn(Schedulers.elastic());
 		parallel.flatMap(deleteProductByID, true).sequential().collectList().block();
-	
+
 		// Delete the category after all products were deleted and throw exceptions if an error occurs.
 		categoryClient.delete().uri("/{id}", id).retrieve().toBodilessEntity().block();
-	
+
 		// When no error occurred return success.
 		return ResponseEntity.noContent().build();
 	}
