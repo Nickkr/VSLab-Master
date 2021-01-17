9478741370bb7d1b4e3d581b95a15d2d5da67450
nkreu <nkreutzarek@hotmail.de>
2021-01-17 17:58:21 +0100

    Fix user service

--- CoreServices/User/src/main/java/com/core/userservice/UserController.java
+++ CoreServices/User/src/main/java/com/core/userservice/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
--- CoreServices/User/src/main/java/com/core/userservice/UserServiceApplication.java
+++ CoreServices/User/src/main/java/com/core/userservice/UserServiceApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@SuppressWarnings("deprecation")
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
