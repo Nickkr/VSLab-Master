f4bdeba445e7bbe37718391d6709c829ea3f8040
Joachim Mees <mejo1031@hs-karlsruhe.de>
2021-01-17 23:29:00 +0100

    Removed warnings suppression for AuthServer/WebSecurityConfig.

--- AuthServer/src/main/java/com/authorization/AuthServer/WebSecurityConfig.java
+++ AuthServer/src/main/java/com/authorization/AuthServer/WebSecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
-@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
